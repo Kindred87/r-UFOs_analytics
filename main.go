be8bd86e52df10c384661d702e576ce9b4e26544
Kindred87 <13651460+Kindred87@users.noreply.github.com>
2024-01-21 17:23:30 -0800

    Add Reddit qualifier

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"os"
 	"ufo_collector/authenticate"
+	"ufo_collector/post"
 )
 
 func main() {
@@ -21,7 +19,8 @@ func main() {
 	totalComments := 0
 	after := ""
 	for i := 0; i < 10; i++ { // get 5 pages of posts
-		posts, nextAfter, err := getRecentPosts(token.AccessToken, after)
+
+		posts, nextAfter, err := post.GetRecent(token.AccessToken, after)
 		if err != nil {
 			fmt.Println("Error getting recent posts:", err)
 			return
@@ -54,13 +53,13 @@ func login() (*authenticate.TokenResponse, error) {
 
 	if _, err := os.Stat("creds.env"); err == nil {
 		// If the file exists
-		id, secret, err = authenticate.GetCredsFromEnv("ANALYTICS_CLIENT_ID", "ANALYTICS_CLIENT_SECRET", "creds.env")
+		id, secret, err = authenticate.GetCredsFromEnv("ANALYTICS_REDDIT_CLIENT_ID", "ANALYTICS_REDDIT_CLIENT_SECRET", "creds.env")
 		if err != nil {
 			log.Fatalf("Error reading credentials: %s", err)
 		}
 	} else if os.IsNotExist(err) {
 		// If the file does not exist
-		id, secret, err = authenticate.GetCredsFromEnv("ANALYTICS_CLIENT_ID", "ANALYTICS_CLIENT_SECRET")
+		id, secret, err = authenticate.GetCredsFromEnv("ANALYTICS_REDDIT_CLIENT_ID", "ANALYTICS_REDDIT_CLIENT_SECRET")
 		if err != nil {
 			log.Fatalf("Error reading credentials: %s", err)
 		}
@@ -75,61 +74,4 @@ func login() (*authenticate.TokenResponse, error) {
 	}
 
 	return token, nil
-}
-
-type Post struct {
-	ID          string `json:"id"`
-	Title       string `json:"title"`
-	URL         string `json:"url"`
-	Flair       string `json:"link_flair_text"`
-	NumComments int    `json:"num_comments"`
-}
-
-type PostListing struct {
-	Data struct {
-		Children []struct {
-			Data Post `json:"data"`
-		} `json:"children"`
-		After string `json:"after"`
-	} `json:"data"`
-}
-
-func getRecentPosts(token string, after string) ([]Post, string, error) {
-	url := "https://oauth.reddit.com/r/UFOs/new?limit=100"
-	if after != "" {
-		url += "&after=" + after
-	}
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, "", err
-	}
-
-	req.Header.Add("Authorization", "Bearer "+token)
-	req.Header.Add("User-Agent", "MyApp/0.0.1")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, "", err
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, "", err
-	}
-
-	var postListing PostListing
-	err = json.Unmarshal(body, &postListing)
-	if err != nil {
-		return nil, "", err
-	}
-
-	posts := make([]Post, len(postListing.Data.Children))
-	for i, child := range postListing.Data.Children {
-		posts[i] = child.Data
-	}
-
-	return posts, postListing.Data.After, nil
 }
---
+++ post/post.go
@@ -0,0 +1,64 @@
+package post
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type Entity struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	URL         string `json:"url"`
+	Flair       string `json:"link_flair_text"`
+	NumComments int    `json:"num_comments"`
+}
+
+type Listing struct {
+	Data struct {
+		Children []struct {
+			Data Entity `json:"data"`
+		} `json:"children"`
+		After string `json:"after"`
+	} `json:"data"`
+}
+
+func GetRecent(token string, after string) ([]Entity, string, error) {
+	url := "https://oauth.reddit.com/r/UFOs/new?limit=100"
+	if after != "" {
+		url += "&after=" + after
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, "", err
+	}
+
+	req.Header.Add("Authorization", "Bearer "+token)
+	req.Header.Add("User-Agent", "MyApp/0.0.1")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, "", err
+	}
+
+	var postListing Listing
+	err = json.Unmarshal(body, &postListing)
+	if err != nil {
+		return nil, "", err
+	}
+
+	posts := make([]Entity, len(postListing.Data.Children))
+	for i, child := range postListing.Data.Children {
+		posts[i] = child.Data
+	}
+
+	return posts, postListing.Data.After, nil
+}
